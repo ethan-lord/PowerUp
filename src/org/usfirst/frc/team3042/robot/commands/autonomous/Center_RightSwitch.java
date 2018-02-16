b60b70a9dbee73a2e25ff333f3c8496a2b5b9ce3
FRCTeam3042 <team3042@gmail.com>
2018-02-15 20:08:02 -0600

    Reads game data, goes to the correct side of the switch and throws a cube in there

--- src/org/usfirst/frc/team3042/robot/commands/autonomous/Center_RightSwitch.java
+++ src/org/usfirst/frc/team3042/robot/commands/autonomous/Center_RightSwitch.java
@@ -1,5 +1,14 @@
 package org.usfirst.frc.team3042.robot.commands.autonomous;
 
+import org.usfirst.frc.team3042.robot.commands.Arm_SetPosition;
+import org.usfirst.frc.team3042.robot.commands.Claw_Unclamp;
+import org.usfirst.frc.team3042.robot.commands.DrivetrainAuton_Drive;
+import org.usfirst.frc.team3042.robot.commands.Elevator_SetPosition;
+import org.usfirst.frc.team3042.robot.paths.CenterToLeftSwitch;
+import org.usfirst.frc.team3042.robot.paths.CenterToRightSwitch;
+import org.usfirst.frc.team3042.robot.subsystems.Arm;
+import org.usfirst.frc.team3042.robot.subsystems.Elevator;
+
 import edu.wpi.first.wpilibj.command.CommandGroup;
 
 /**
@@ -18,6 +27,11 @@ public class Center_RightSwitch extends CommandGroup {
         // e.g. addParallel(new Command1());
         //      addSequential(new Command2());
         // Command1 and Command2 will run in parallel.
+    	
+    	addParallel(new Elevator_SetPosition(Elevator.Position.SWITCH));
+    	addParallel(new Arm_SetPosition(Arm.Position.MIDDLE));
+    	addSequential(new DrivetrainAuton_Drive(new CenterToRightSwitch().buildPath()));
+    	addSequential(new Claw_Unclamp());
 
         // A command group will require all of the subsystems that each member
         // would require.
