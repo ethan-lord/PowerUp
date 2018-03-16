a834406523ab8c7c88221c9552667a9cc0c419cf
Ethan Lord <Thardro@users.noreply.github.com>
2018-03-15 20:54:59 -0500

    Finished PoseTracker, needs testing

--- src/org/usfirst/frc/team3042/robot/RobotPoseTracker.java
+++ src/org/usfirst/frc/team3042/robot/RobotPoseTracker.java
@@ -1,9 +1,12 @@
 package org.usfirst.frc.team3042.robot;
 
+import org.usfirst.frc.team3042.lib.math.Kinematics;
 import org.usfirst.frc.team3042.lib.math.RigidTransform2d;
 import org.usfirst.frc.team3042.lib.math.Rotation2d;
+import org.usfirst.frc.team3042.lib.math.Twist2d;
 
 import edu.wpi.first.wpilibj.Notifier;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class RobotPoseTracker {
 	RobotPoseTracker instance = null;
@@ -26,6 +29,7 @@ public class RobotPoseTracker {
 	};
 	
 	private RigidTransform2d pose;
+	private Twist2d measuredVelocity;
 	
 	private double oldLeftPos, oldRightPos;
 	private Rotation2d oldHeading;
@@ -48,10 +52,29 @@ public class RobotPoseTracker {
 		Rotation2d currentHeading = Robot.drivetrain.getGyro();
 		Rotation2d dHeading = oldHeading.inverse().rotateBy(currentHeading);
 		
+		final Twist2d odometryVelocity = generateOdometry(dLeftPos, dRightPos, currentHeading);
 		
+		pose = Kinematics.integrateForwardKinematics(pose, odometryVelocity);
+		measuredVelocity = odometryVelocity;
 		
 		oldLeftPos = leftPos;
 		oldRightPos = rightPos;
+	}
+	
+	private Twist2d generateOdometry(double dLeftPos, double dRightPos, Rotation2d currentHeading) {
+		final RigidTransform2d lastPosition = getPose();
+		final Twist2d delta = Kinematics.forwardKinematics(lastPosition.getRotation(),
+                dLeftPos, dRightPos, currentHeading);
+		
+		return delta;
+	}
+	
+	public void outputToDashboard() {
+		RigidTransform2d odometry = getPose();
+        SmartDashboard.putNumber("robot_pose_x", odometry.getTranslation().x());
+        SmartDashboard.putNumber("robot_pose_y", odometry.getTranslation().y());
+        SmartDashboard.putNumber("robot_pose_theta", odometry.getRotation().getDegrees());
+        SmartDashboard.putNumber("robot velocity", measuredVelocity.dx);
 	}
 	
 }
